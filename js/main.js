c6fd457f420c8b96b4adccb6f21b07195469145d
thegrep <den.belotserkovets@gmail.com>
2018-10-17 12:44:21 +0300

    add popup add fade effect to carousel

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
-    });
+
     $('.crsl').slick({
         dots: false,
         arrows: false,
@@ -12,6 +7,16 @@ $(document).ready(function(){
         speed: 300,
         slidesToShow: 1,
         autoplay: true,
-        autoplaySpeed: 1500
+        autoplaySpeed: 1500,
+        fade: true
+    });
+
+
+
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
     });
 });
